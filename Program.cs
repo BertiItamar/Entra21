026ab923293e00b8e054c074f912590f1e71f867
Itamar Berti De Souza <114452919+BertiItamar@users.noreply.github.com>
2022-10-05 21:34:38 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+      
+            Menu menu = new Menu();
+            menu.MostrarMenuInicial();
+
+
+            
+
+        }
+    }
+}
